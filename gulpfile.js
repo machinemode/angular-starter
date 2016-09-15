754b040bce0c44fd91b5b62d9f35639bb34e5623
Cesar Carrasco <cesar@iruleathome.com>
2016-09-14 22:04:08 -0400

    added gulp and gulp-sass

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+gulp.task('sass', () => {
+	return new Promise((resolve, reject) => {
+		gulp.src(`${__dirname}/src/**/*.scss`)
+			.pipe(sass({
+				outputStyle: 'nested',
+				sourceMapEmbed: false
+			}))
+			.on('error', reject)
+			.pipe(gulp.dest(`${__dirname}/src`))
+			.on('end', resolve);
+	});
+});
+
+gulp.task('watch', ['sass'], () => {
+	gulp.watch(`${__dirname}/src/**/*.scss`, ['sass']);
+});
